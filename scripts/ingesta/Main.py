2c7875b96edde446393034ebbfa4db4e35afcd93
VitorPaes <vitor.mor66@live.com>
2020-10-18 23:04:36 -0300

    Ajuste

--- scripts/ingesta/Main.py
+++ scripts/ingesta/Main.py
@@ -1,15 +1,21 @@
+#- * -coding: utf - 8 - * -
+
 import pyspark
 import sys
 from google.cloud import storage
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql import HiveContext
+from pyspark.sql import HiveContext 
 import logging
+
+
+
 
 spark = SparkSession \
     .builder \
-    .appName("Load_And_WRITe_TABLES") \
+    .appName("LOAD_AND_WRITE_TABLES") \
     .config('spark.sql.parquet.compression.codec', 'snappy') \
+    .enableHiveSupport() \
     .getOrCreate()
 
 bill_of_materials = "gs://dados_boavista/TesteEngenheiroDados/bill_of_materials.csv"
@@ -31,8 +37,22 @@ def main():
         df_comp_bosss = read_csv_file(comp_boss)
         df_price_quote = read_csv_file(price_quote)
         #df.show()
+
+
+
+        print("::::::: INICIANDO A GRAVAÇÃO NAS TABELAS HIVE ::::::")
+        df_bill_of_materials.write.mode("overwrite").format("parquet").option("compression","snappy").insertInto("db.bill_of_materials")
+        df_comp_bosss.write.mode("overwrite").format("parquet").option("compression","snappy").insertInto("db.comp_boss")
+        df_price_quote.write.mode("overwrite").format("parquet").option("compression","snappy").insertInto("db.price_quote")
+
+
+        print("::::::: TABELAS GRAVADAS ::::::")
+
+        
                
     except Exception as ex:
+        print("::::::: ERRO ::::::")
+
         print(ex)
 
 
